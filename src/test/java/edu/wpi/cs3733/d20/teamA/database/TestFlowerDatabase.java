b7d45956a9ca4a8d4961792bd796d219e3bb4485
Eva <elabbe@wpi.edu>
2020-04-18 16:19:19 -0400

    I typed git in the code by accident

--- src/test/java/edu/wpi/cs3733/d20/teamA/database/TestFlowerDatabase.java
+++ src/test/java/edu/wpi/cs3733/d20/teamA/database/TestFlowerDatabase.java
@@ -115,7 +115,7 @@ public class TestFlowerDatabase {
     Assertions.assertFalse(b);
     fDB.addOrder(2, "Daisy", "Blue", "buiscuit");
     fDB.addOrder(6, "Daisy", "Blue", "biscuit");
-    boolean r = fDB.deleteOrder(2git);
+    boolean r = fDB.deleteOrder(2);
     Assertions.assertTrue(r);
     boolean s = fDB.helperEmpty("SELECT * FROM Orders");
     Assertions.assertTrue(s);
