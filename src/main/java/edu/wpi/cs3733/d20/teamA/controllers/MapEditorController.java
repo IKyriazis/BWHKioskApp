b5bac7480bace8161270e597cb57c90a8e5300cd
Gabriel Dudlicek <g.dudlicek@gmail.com>
2020-04-19 13:03:37 -0500

    added node info btn, added tim's run lines to build.gradle

--- src/main/java/edu/wpi/cs3733/d20/teamA/controllers/MapEditorController.java
+++ src/main/java/edu/wpi/cs3733/d20/teamA/controllers/MapEditorController.java
@@ -3,15 +3,30 @@ package edu.wpi.cs3733.d20.teamA.controllers;
 import com.jfoenix.controls.JFXSlider;
 import edu.wpi.cs3733.d20.teamA.map.MapCanvas;
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Cursor;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 
 public class MapEditorController {
   @FXML private AnchorPane canvasPane;
   @FXML private JFXSlider zoomSlider;
+  @FXML private Label nodeInfoLbl;
 
   private MapCanvas canvas;
+
+  enum Mode {
+    nodeInfo,
+    add,
+    edit,
+    delete,
+    uploadCSV,
+    downloadCSV
+  }
+
+  // Setup the Mode
+  Mode mode = Mode.nodeInfo;
 
   @FXML
   public void initialize() {
@@ -28,5 +43,10 @@ public class MapEditorController {
     zoomSlider.setCursor(Cursor.H_RESIZE);
 
     Platform.runLater(() -> canvas.draw(1));
+  }
+
+  public void nodeInfoPressed(ActionEvent actionEvent) {
+    mode = Mode.nodeInfo;
+    nodeInfoLbl.setText("Info for nodes goes here");
   }
 }
