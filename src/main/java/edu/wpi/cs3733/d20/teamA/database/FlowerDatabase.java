02dac77bef256146d3de35da608bc725a1ffb8c2
Eva <elabbe@wpi.edu>
2020-04-19 15:40:16 -0400

    Two new methods for UI to test

--- src/main/java/edu/wpi/cs3733/d20/teamA/database/FlowerDatabase.java
+++ src/main/java/edu/wpi/cs3733/d20/teamA/database/FlowerDatabase.java
@@ -358,4 +358,68 @@ public class FlowerDatabase extends Database {
     }
   }
 
+  public double getFlowerPricePer(String type, String color){
+    double price = -1;
+    try {
+      PreparedStatement pstmt = getConnection().prepareStatement("Select pricePer From Flowers Where typeFlower = '"+type+"' AND color = '"+color+"'");
+      ResultSet rset = pstmt.executeQuery();
+      while (rset.next()) {
+        price = rset.getInt("pricePer");
+      }
+      rset.close();
+      pstmt.close();
+      return price;
+    } catch (SQLException e) {
+      return -1;
+    }
+  }
+
+  public String getOrderStatus(int orderNum){
+    String status = null;
+    try {
+      PreparedStatement pstmt = getConnection().prepareStatement("Select status From Orders Where orderNumber = '"+orderNum+"'");
+      ResultSet rset = pstmt.executeQuery();
+      while (rset.next()) {
+        status = rset.getString("status");
+      }
+      rset.close();
+      pstmt.close();
+      return status;
+    } catch (SQLException e) {
+      return null;
+    }
+  }
+
+  /*
+  public ObservableList<Order> orderSentOl(int orderNum) throws SQLException {
+    ObservableList<Order> oList = FXCollections.observableArrayList();
+    try {
+      Connection conn = DriverManager.getConnection("jdbc:derby:BWDatabase");
+      PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM Orders WHERE orderNumber = ");
+      ResultSet rset = pstmt.executeQuery();
+      while (rset.next()) {
+        int orderNumber = rset.getInt("orderNumber");
+        int numFlowers = rset.getInt("numFlowers");
+        String flowerType = rset.getString("flowerType");
+        String flowerColor = rset.getString("flowerColor");
+        double price = rset.getDouble("price");
+        String status = rset.getString("status");
+        String location = rset.getString("location");
+
+        Order node =
+                new Order(orderNumber, numFlowers, flowerType, flowerColor, price, status, location);
+        oList.add(node);
+      }
+      rset.close();
+      pstmt.close();
+      conn.close();
+      return oList;
+    } catch (SQLException e) {
+      e.printStackTrace();
+      return oList;
+    }
+  }
+
+   */
+
 }
