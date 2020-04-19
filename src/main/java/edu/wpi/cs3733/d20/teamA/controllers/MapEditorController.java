8a809fdf0b601c8b64218f7f1bb462520025fe1b
Gabriel Dudlicek <g.dudlicek@gmail.com>
2020-04-19 13:26:36 -0500

    added canvas clicked method and displaying node info

--- src/main/java/edu/wpi/cs3733/d20/teamA/controllers/MapEditorController.java
+++ src/main/java/edu/wpi/cs3733/d20/teamA/controllers/MapEditorController.java
@@ -1,12 +1,16 @@
 package edu.wpi.cs3733.d20.teamA.controllers;
 
 import com.jfoenix.controls.JFXSlider;
+import edu.wpi.cs3733.d20.teamA.graph.Node;
 import edu.wpi.cs3733.d20.teamA.map.MapCanvas;
+import java.util.Optional;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Point2D;
 import javafx.scene.Cursor;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
 public class MapEditorController {
@@ -39,6 +43,9 @@ public class MapEditorController {
     // Setup zoom slider hook
     canvas.getZoomProperty().bindBidirectional(zoomSlider.valueProperty());
 
+    // Setup the canvasClicked function
+    canvas.setOnMouseClicked(this::canvasClicked);
+
     // Setup zoom slider cursor
     zoomSlider.setCursor(Cursor.H_RESIZE);
 
@@ -47,6 +54,23 @@ public class MapEditorController {
 
   public void nodeInfoPressed(ActionEvent actionEvent) {
     mode = Mode.nodeInfo;
-    nodeInfoLbl.setText("Info for nodes goes here");
+  }
+
+  public void canvasClicked(MouseEvent mouse) {
+    Point2D mousePos = new Point2D(mouse.getX(), mouse.getY());
+
+    if (mode == Mode.nodeInfo) {
+      Optional<Node> node = canvas.getClosestNode(mousePos, 15);
+      if (node.isPresent()) {
+        Node closestNode = node.get();
+        nodeInfoLbl.setText(
+            "Node ID: "
+                + closestNode.getNodeID()
+                + "\nX: "
+                + closestNode.getX()
+                + "\nY: "
+                + closestNode.getY());
+      }
+    }
   }
 }
