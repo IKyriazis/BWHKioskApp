aba5622c853cbbc94c2ba494665c26607501b71d
dgcole <dyllancole@outlook.com>
2020-04-19 21:42:58 -0400

    Map redraw on tab switch

--- src/main/java/edu/wpi/cs3733/d20/teamA/controllers/SimpleMapController.java
+++ src/main/java/edu/wpi/cs3733/d20/teamA/controllers/SimpleMapController.java
@@ -8,6 +8,7 @@ import edu.wpi.cs3733.d20.teamA.graph.Node;
 import edu.wpi.cs3733.d20.teamA.graph.Path;
 import edu.wpi.cs3733.d20.teamA.map.MapCanvas;
 import edu.wpi.cs3733.d20.teamA.util.NodeAutoCompleteHandler;
+import edu.wpi.cs3733.d20.teamA.util.TabSwitchEvent;
 import java.util.Comparator;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -20,9 +21,11 @@ import javafx.scene.Cursor;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 
 public class SimpleMapController {
+  @FXML private BorderPane rootPane;
   @FXML private JFXDrawer directionsDrawer;
   @FXML private VBox directionsBox;
   @FXML private JFXComboBox<Node> startingLocationBox;
@@ -80,6 +83,13 @@ public class SimpleMapController {
         event -> {
           drawPathButton.setSelected(false);
           canvas.setDrawAllNodes(true);
+          canvas.draw(1);
+        });
+
+    // Register event handler to redraw map on tab selection
+    rootPane.addEventHandler(
+        TabSwitchEvent.TAB_SWITCH,
+        event -> {
           canvas.draw(1);
         });
 
