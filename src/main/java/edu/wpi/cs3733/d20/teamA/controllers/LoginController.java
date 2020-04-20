982a3e53ae87fe1d097a523a080f75760b700795
YashPatel5 <yppatel@wpi.edu>
2020-04-19 20:48:55 -0400

    Got the Required Test coverage for Login Controller

--- src/main/java/edu/wpi/cs3733/d20/teamA/controllers/LoginController.java
+++ src/main/java/edu/wpi/cs3733/d20/teamA/controllers/LoginController.java
@@ -1,5 +1,6 @@
 package edu.wpi.cs3733.d20.teamA.controllers;
 
+import com.google.inject.Inject;
 import com.jfoenix.controls.JFXTabPane;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
@@ -7,6 +8,7 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.effect.GaussianBlur;
@@ -20,6 +22,19 @@ public class LoginController {
   @FXML private JFXTabPane tabPane;
 
   private GaussianBlur currentBlur;
+
+  private Scene appPrimaryScene;
+
+  /**
+   * This method allows the tests to inject the scene at a later time, since it must be done on the
+   * JavaFX thread
+   *
+   * @param appPrimaryScene Primary scene of the app whose root will be changed
+   */
+  @Inject
+  public void setAppPrimaryScene(Scene appPrimaryScene) {
+    this.appPrimaryScene = appPrimaryScene;
+  }
 
   @FXML
   public void initialize() {
