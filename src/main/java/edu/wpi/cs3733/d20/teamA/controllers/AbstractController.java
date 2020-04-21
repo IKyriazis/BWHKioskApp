d6f8bb309787d6c4383aeb648081ed4dc5def571
YashPatel5 <yppatel@wpi.edu>
2020-04-21 14:15:47 -0400

    Removed Duplicated instance of Database

--- src/main/java/edu/wpi/cs3733/d20/teamA/controllers/AbstractController.java
+++ src/main/java/edu/wpi/cs3733/d20/teamA/controllers/AbstractController.java
@@ -24,13 +24,12 @@ public abstract class AbstractController {
       graphDatabase = new GraphDatabase(conn);
       graphDatabase.createTables();
       flDatabase = new FlowerDatabase(conn);
+      flDatabase.createTables();
       eDB = new EmployeesDatabase(conn);
       eDB.createTables();
       graphDatabase.deleteNode("ID");
       janitorDatabase = new JanitorDatabase(conn);
       janitorDatabase.createTables();
-      eDB = new EmployeesDatabase(conn);
-      eDB.createTables();
     } catch (SQLException throwables) {
       throwables.printStackTrace(); // All is lost
     }
