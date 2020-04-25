bfcafe638d6c7b2ae1462c10fd9cde866b750a6c
tclooney <tclooney@wpi.edu>
2020-04-24 23:33:31 -0400

    Created the Context class that allows for the switching of algorithms

---
+++ src/main/java/edu/wpi/cs3733/d20/teamA/graph/ContextPath.java
@@ -0,0 +1,21 @@
+package edu.wpi.cs3733.d20.teamA.graph;
+
+public class ContextPath {
+
+    private IStrategyPath pathFindingAlgo;
+
+    public ContextPath(){}
+
+    public ContextPath(IStrategyPath pathFindingAlgo){
+        this.pathFindingAlgo = pathFindingAlgo;
+    }
+
+    public void setContextPath(IStrategyPath pathFindingAlgo){
+        this.pathFindingAlgo = pathFindingAlgo;
+    }
+
+    public void findPath(Node start, Node end){
+        pathFindingAlgo.findPath(start, end);
+    }
+
+}
