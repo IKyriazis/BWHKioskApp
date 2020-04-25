9f901593755d6c4900f50e9379381cfd5841e595
ikyriazis <ikyriazis@wpi.edu>
2020-04-25 13:22:26 -0400

    Added InternalTransportDatabase functions

--- src/main/java/edu/wpi/cs3733/d20/teamA/database/InternalTransportDatabase.java
+++ src/main/java/edu/wpi/cs3733/d20/teamA/database/InternalTransportDatabase.java
@@ -1,5 +1,87 @@
 package edu.wpi.cs3733.d20.teamA.database;
 
-public class InternalTransportDatabase {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+
+public class InternalTransportDatabase extends Database {
+
+    private int requestCount = 0;
+
+    public InternalTransportDatabase(Connection connection) {
+        super(connection);
+
+        if (doesTableNotExist("InternalTransportRequest")) {
+            createTables();
+        }
+    }
+
+    /**
+     * Creates internal transport tables
+     *
+     * @return False if tables couldn't be created
+     */
+    public boolean createTables() {
+
+        // Create the janitorrequest table
+        return helperPrepared(
+                "CREATE TABLE InternalTransportRequest (requestNumber INTEGER PRIMARY KEY, time TIMESTAMP NOT NULL, start Varchar(10) NOT NULL, destination Varchar(10) NOT NULL, name Varchar(15), progress Varchar(19) NOT NULL, CONSTRAINT FK_S FOREIGN KEY (start) REFERENCES Node(nodeID), CONSTRAINT FK_D FOREIGN KEY (destination) REFERENCES Node(nodeID), CONSTRAINT CHK_PROG CHECK (progress in ('Reported', 'Dispatched', 'Done')))");
+    }
+
+    /**
+     * Drops the internal transport request tables so we can start fresh
+     *
+     * @return false if the tables don't exist, true if table is dropped correctly
+     */
+    public boolean dropTables() {
+
+        // if the helper returns false this method should too
+        // drop the CONSTRAINT first
+        if (!(helperPrepared("ALTER TABLE InternalTransportRequest DROP CONSTRAINT FK_S") && helperPrepared("ALTER TABLE InternalTransportRequest DROP CONSTRAINT FK_D"))) {
+
+            return false;
+        }
+        // Drop the tables
+        return helperPrepared("DROP TABLE InternalTransportRequest");
+    }
+
+    /**
+     * Sets the requestCount to 0
+     *
+     * @return true if everything could be deleted
+     */
+    public boolean removeAll() {
+        requestCount = 0;
+        return helperPrepared("DELETE From InternalTransportRequest");
+    }
+
+    public boolean addRequest(String start, String destination) {
+        // creates a timestamp of the time that the function is called
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        // default status is reported
+        String progress = "Reported";
+        try {
+            // creates the prepared statement that will be sent to the database
+            PreparedStatement pstmt =
+                    getConnection()
+                            .prepareStatement(
+                                    "INSERT INTO InternalTransportRequest (time, start, progress, destination, requestNumber) VALUES (?, ?, ?, ?, ?)");
+            // sets all the parameters of the prepared statement string
+            pstmt.setTimestamp(1, timestamp);
+            pstmt.setString(2, start);
+            pstmt.setString(3, progress);
+            pstmt.setString(4, destination);
+            // first request starts at 1 and increments every time a new request is added
+            pstmt.setInt(5, ++requestCount);
+            pstmt.executeUpdate();
+            pstmt.close();
+            // return true if the request is added
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 
 }
