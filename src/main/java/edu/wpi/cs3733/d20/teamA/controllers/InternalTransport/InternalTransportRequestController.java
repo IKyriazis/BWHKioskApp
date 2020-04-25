b5cce8d465ebcf13598612bcdad1bea02433feff
ikyriazis <ikyriazis@wpi.edu>
2020-04-25 15:50:50 -0400

    Corrected initialize method in request controller

--- src/main/java/edu/wpi/cs3733/d20/teamA/controllers/InternalTransport/InternalTransportRequestController.java
+++ src/main/java/edu/wpi/cs3733/d20/teamA/controllers/InternalTransport/InternalTransportRequestController.java
@@ -6,13 +6,19 @@ import com.jfoenix.controls.JFXDialog;
 import edu.wpi.cs3733.d20.teamA.controllers.AbstractController;
 import edu.wpi.cs3733.d20.teamA.controllers.dialog.IDialogController;
 import edu.wpi.cs3733.d20.teamA.database.Flower;
+import edu.wpi.cs3733.d20.teamA.graph.Graph;
 import edu.wpi.cs3733.d20.teamA.graph.Node;
 import edu.wpi.cs3733.d20.teamA.util.DialogUtil;
+import edu.wpi.cs3733.d20.teamA.util.InputFormatUtil;
+import edu.wpi.cs3733.d20.teamA.util.NodeAutoCompleteHandler;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
+import java.util.Comparator;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class InternalTransportRequestController extends AbstractController implements IDialogController {
     @FXML private JFXComboBox<Node> startList;
@@ -21,7 +27,26 @@ public class InternalTransportRequestController extends AbstractController imple
     private JFXDialog dialog;
 
     @FXML
-    public void placeOrder(ActionEvent actionEvent) {
+    public void initialize() throws Exception {
+        // Setup list of destination nodes
+        ObservableList<Node> allNodeList =
+                FXCollections.observableArrayList(
+                        Graph.getInstance().getNodes().values().stream()
+                                .filter(node -> node.getFloor() == 1)
+                                .collect(Collectors.toList()));
+        allNodeList.sort(Comparator.comparing(Node::getLongName));
+        startList.setItems(allNodeList);
+        startList
+                .getEditor()
+                .setOnKeyTyped(new NodeAutoCompleteHandler(startList, startList, allNodeList));
+        destinationList.setItems(allNodeList);
+        destinationList
+                .getEditor()
+                .setOnKeyTyped(new NodeAutoCompleteHandler(destinationList, destinationList, allNodeList));
+    }
+
+    @FXML
+    public void placeRequest(ActionEvent actionEvent) {
         if (choiceFlower.getSelectionModel().getSelectedItem() != null) {
             String s = choiceFlower.getSelectionModel().getSelectedItem();
             String type = s.substring(0, s.indexOf(','));
