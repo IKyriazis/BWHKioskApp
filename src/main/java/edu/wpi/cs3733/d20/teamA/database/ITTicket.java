85809cc595d615d53f9b68d4cab281d27702d2b5
Gabriel Dudlicek <g.dudlicek@gmail.com>
2020-04-25 14:47:52 -0500

    added ticket number

--- src/main/java/edu/wpi/cs3733/d20/teamA/database/ITTicket.java
+++ src/main/java/edu/wpi/cs3733/d20/teamA/database/ITTicket.java
@@ -2,99 +2,116 @@ package edu.wpi.cs3733.d20.teamA.database;
 
 import com.jfoenix.controls.JFXTreeTableColumn;
 import edu.wpi.cs3733.d20.teamA.controls.ITableable;
-import javafx.beans.property.SimpleDoubleProperty;
+import java.util.ArrayList;
+import java.util.List;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import lombok.Setter;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class ITTicket implements ITableable<ITTicket> {
 
-    @Setter
-    private SimpleStringProperty name;
-    @Setter
-    private SimpleStringProperty location;
-    @Setter
-    private SimpleStringProperty category;
-    @Setter
-    private SimpleStringProperty description;
-    @Setter
-    private SimpleStringProperty status;
-    @Setter
-    private SimpleStringProperty completedBy;
+  @Setter private SimpleIntegerProperty ticketNum;
+  @Setter private SimpleStringProperty name;
+  @Setter private SimpleStringProperty location;
+  @Setter private SimpleStringProperty category;
+  @Setter private SimpleStringProperty description;
+  @Setter private SimpleStringProperty status;
+  @Setter private SimpleStringProperty completedBy;
 
-    public ITTicket(SimpleStringProperty name, SimpleStringProperty location, SimpleStringProperty category, SimpleStringProperty description, SimpleStringProperty status, SimpleStringProperty completedBy) {
-        this.name = name;
-        this.location = location;
-        this.category = category;
-        this.description = description;
-        this.status = status;
-        this.completedBy = completedBy;
-    }
+  public ITTicket(
+      int ticketNum,
+      String name,
+      String location,
+      String category,
+      String description,
+      String status,
+      String completedBy) {
+    this.ticketNum = new SimpleIntegerProperty(ticketNum);
+    this.name = new SimpleStringProperty(name);
+    this.location = new SimpleStringProperty(location);
+    this.category = new SimpleStringProperty(category);
+    this.description = new SimpleStringProperty(description);
+    this.status = new SimpleStringProperty(status);
+    this.completedBy = new SimpleStringProperty(completedBy);
+  }
 
-    public String getName() {
-        return name.get();
-    }
+  public int getTicketNum() {
+    return ticketNum.get();
+  }
 
-    public SimpleStringProperty nameProperty() {
-        return name;
-    }
+  public SimpleIntegerProperty ticketNumProperty() {
+    return ticketNum;
+  }
 
-    public String getLocation() {
-        return location.get();
-    }
+  public String getName() {
+    return name.get();
+  }
 
-    public SimpleStringProperty locationProperty() {
-        return location;
-    }
+  public SimpleStringProperty nameProperty() {
+    return name;
+  }
 
-    public String getCategory() {
-        return category.get();
-    }
+  public String getLocation() {
+    return location.get();
+  }
 
-    public SimpleStringProperty categoryProperty() {
-        return category;
-    }
+  public SimpleStringProperty locationProperty() {
+    return location;
+  }
 
-    public String getDescription() {
-        return description.get();
-    }
+  public String getCategory() {
+    return category.get();
+  }
 
-    public SimpleStringProperty descriptionProperty() {
-        return description;
-    }
+  public SimpleStringProperty categoryProperty() {
+    return category;
+  }
 
-    public String getStatus() {
-        return status.get();
-    }
+  public String getDescription() {
+    return description.get();
+  }
 
-    public SimpleStringProperty statusProperty() {
-        return status;
-    }
+  public SimpleStringProperty descriptionProperty() {
+    return description;
+  }
 
-    public String getCompletedBy() {
-        return completedBy.get();
-    }
+  public String getStatus() {
+    return status.get();
+  }
 
-    public SimpleStringProperty completedByProperty() {
-        return completedBy;
-    }
+  public SimpleStringProperty statusProperty() {
+    return status;
+  }
 
-    @Override
-    public ArrayList<JFXTreeTableColumn<ITTicket, ?>> getColumns() {
-        JFXTreeTableColumn<ITTicket, String> column1 = new JFXTreeTableColumn<>("Status");
-        column1.setCellValueFactory(param -> param.getValue().getValue().statusProperty());
-        JFXTreeTableColumn<ITTicket, String> column2 = new JFXTreeTableColumn<>("Category");
-        column2.setCellValueFactory(param -> param.getValue().getValue().categoryProperty());
-        JFXTreeTableColumn<ITTicket, String> column3 = new JFXTreeTableColumn<>("Location");
-        column3.setCellValueFactory(param -> param.getValue().getValue().locationProperty());
-        JFXTreeTableColumn<ITTicket, String> column4 = new JFXTreeTableColumn<>("Requester Name");
-        column4.setCellValueFactory(param -> param.getValue().getValue().nameProperty());
-        JFXTreeTableColumn<ITTicket, String> column5 = new JFXTreeTableColumn<>("Employee Name");
-        column5.setCellValueFactory(param -> param.getValue().getValue().completedByProperty());
+  public String getCompletedBy() {
+    return completedBy.get();
+  }
 
-        return new ArrayList<>(List.of(column1, column2, column3, column4, column5));
-    }
+  public SimpleStringProperty completedByProperty() {
+    return completedBy;
+  }
+
+  @Override
+  public ArrayList<JFXTreeTableColumn<ITTicket, ?>> getColumns() {
+    JFXTreeTableColumn<ITTicket, Integer> column1 = new JFXTreeTableColumn<>("Ticket #");
+    column1.setCellValueFactory(
+        param -> param.getValue().getValue().ticketNumProperty().asObject());
+
+    JFXTreeTableColumn<ITTicket, String> column2 = new JFXTreeTableColumn<>("Status");
+    column2.setCellValueFactory(param -> param.getValue().getValue().statusProperty());
+
+    JFXTreeTableColumn<ITTicket, String> column3 = new JFXTreeTableColumn<>("Category");
+    column3.setCellValueFactory(param -> param.getValue().getValue().categoryProperty());
+
+    JFXTreeTableColumn<ITTicket, String> column4 = new JFXTreeTableColumn<>("Location");
+    column4.setCellValueFactory(param -> param.getValue().getValue().locationProperty());
+
+    JFXTreeTableColumn<ITTicket, String> column5 = new JFXTreeTableColumn<>("Requester Name");
+    column5.setCellValueFactory(param -> param.getValue().getValue().nameProperty());
+
+    JFXTreeTableColumn<ITTicket, String> column6 = new JFXTreeTableColumn<>("Employee Name");
+    column6.setCellValueFactory(param -> param.getValue().getValue().completedByProperty());
+
+    return new ArrayList<>(List.of(column1, column2, column3, column4, column5, column6));
+  }
 }
