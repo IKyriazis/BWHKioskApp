2cbdd1076bec7acbc6342a6cbc6b77ff9cc46490
tclooney <tclooney@wpi.edu>
2020-04-25 13:32:02 -0400

    Setup basics of the PatientInfoController

--- src/main/java/edu/wpi/cs3733/d20/teamA/controllers/PatientInfoController.java
+++ src/main/java/edu/wpi/cs3733/d20/teamA/controllers/PatientInfoController.java
@@ -1,21 +1,33 @@
 package edu.wpi.cs3733.d20.teamA.controllers;
 
 
+import edu.wpi.cs3733.d20.teamA.controls.SimpleTableView;
+
+import edu.wpi.cs3733.d20.teamA.database.Patient;
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+
 public class PatientInfoController extends AbstractController {
+    @FXML
+    private GridPane patientServiceTablePane;
+    private SimpleTableView<Patient> patientTable;
 
     public PatientInfoController(){}
 
     public void initialize(){
-        if (flDatabase.getSizeFlowers() == -1) {
-            flDatabase.dropTables();
-            flDatabase.createTables();
-            flDatabase.readFlowersCSV();
-            flDatabase.readFlowerOrderCSV();
-        } else if (flDatabase.getSizeFlowers() == 0 || flDatabase.getSizeOrders() == 0) {
-            flDatabase.removeAllFlowers();
-            flDatabase.removeAllOrders();
-            flDatabase.readFlowersCSV();
-            flDatabase.readFlowerOrderCSV();
+        if (patientDatabase.getSizePatients() == -1) {
+            patientDatabase.dropTables();
+            patientDatabase.createTables();
+            //patientDatabase.readPatientCSV();
+        } else if (patientDatabase.getSizePatients() == 0) {
+            patientDatabase.removeAllPatients();
+            //patientDatabase.readFlowersCSV();
         }
+
+        // Setup any icons we want to use
+
+        // Setup Table
+        patientTable = new SimpleTableView<>(new Patient("", "", "", "", 0, 0, 0.0, "", "", ""), 20.0);
+        patientServiceTablePane.getChildren().add(patientTable);
     }
 }
