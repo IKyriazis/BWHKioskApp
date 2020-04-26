a3b7ff849b40ba617c85cc993f19682c7b83b3b3
Gabriel Dudlicek <g.dudlicek@gmail.com>
2020-04-25 20:03:48 -0500

    made the database functions for the ITTickets

--- src/main/java/edu/wpi/cs3733/d20/teamA/database/ITTicket.java
+++ src/main/java/edu/wpi/cs3733/d20/teamA/database/ITTicket.java
@@ -2,131 +2,126 @@ package edu.wpi.cs3733.d20.teamA.database;
 
 import com.jfoenix.controls.JFXTreeTableColumn;
 import edu.wpi.cs3733.d20.teamA.controls.ITableable;
+
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
+
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import lombok.Setter;
 
 public class ITTicket implements ITableable<ITTicket> {
 
-  @Setter private SimpleIntegerProperty ticketNum;
-  @Setter private SimpleStringProperty ticketTime;
-  @Setter private SimpleStringProperty name;
-  @Setter private SimpleStringProperty location;
-  @Setter private SimpleStringProperty category;
-  @Setter private SimpleStringProperty description;
-  @Setter private SimpleStringProperty status;
-  @Setter private SimpleStringProperty completedBy;
+    @Setter
+    private SimpleStringProperty ticketTime;
+    @Setter
+    private SimpleStringProperty name;
+    @Setter
+    private SimpleStringProperty location;
+    @Setter
+    private SimpleStringProperty category;
+    @Setter
+    private SimpleStringProperty description;
+    @Setter
+    private SimpleStringProperty status;
+    @Setter
+    private SimpleStringProperty completedBy;
 
-  public ITTicket(
-      int ticketNum,
-      String name,
-      Timestamp ticketTime,
-      String location,
-      String category,
-      String description,
-      String status,
-      String completedBy) {
-    this.ticketNum = new SimpleIntegerProperty(ticketNum);
-    this.ticketTime = new SimpleStringProperty(ticketTime.toString());
-    this.name = new SimpleStringProperty(name);
-    this.location = new SimpleStringProperty(location);
-    this.category = new SimpleStringProperty(category);
-    this.description = new SimpleStringProperty(description);
-    this.status = new SimpleStringProperty(status);
-    this.completedBy = new SimpleStringProperty(completedBy);
-  }
+    public ITTicket(
+            Timestamp ticketTime,
+            String location,
+            String category,
+            String status,
+            String name,
+            String completedBy,
+            String description) {
+        this.ticketTime = new SimpleStringProperty(ticketTime.toString());
+        this.name = new SimpleStringProperty(name);
+        this.location = new SimpleStringProperty(location);
+        this.category = new SimpleStringProperty(category);
+        this.description = new SimpleStringProperty(description);
+        this.status = new SimpleStringProperty(status);
+        this.completedBy = new SimpleStringProperty(completedBy);
+    }
 
-  public String getticketTime() {
-    return ticketTime.get();
-  }
+    public String getticketTime() {
+        return ticketTime.get();
+    }
 
-  public SimpleStringProperty ticketTimeProperty() {
-    return ticketTime;
-  }
+    public SimpleStringProperty ticketTimeProperty() {
+        return ticketTime;
+    }
 
-  public int getTicketNum() {
-    return ticketNum.get();
-  }
+    public String getName() {
+        return name.get();
+    }
 
-  public SimpleIntegerProperty ticketNumProperty() {
-    return ticketNum;
-  }
+    public SimpleStringProperty nameProperty() {
+        return name;
+    }
 
-  public String getName() {
-    return name.get();
-  }
+    public String getLocation() {
+        return location.get();
+    }
 
-  public SimpleStringProperty nameProperty() {
-    return name;
-  }
+    public SimpleStringProperty locationProperty() {
+        return location;
+    }
 
-  public String getLocation() {
-    return location.get();
-  }
+    public String getCategory() {
+        return category.get();
+    }
 
-  public SimpleStringProperty locationProperty() {
-    return location;
-  }
+    public SimpleStringProperty categoryProperty() {
+        return category;
+    }
 
-  public String getCategory() {
-    return category.get();
-  }
+    public String getDescription() {
+        return description.get();
+    }
 
-  public SimpleStringProperty categoryProperty() {
-    return category;
-  }
+    public SimpleStringProperty descriptionProperty() {
+        return description;
+    }
 
-  public String getDescription() {
-    return description.get();
-  }
+    public String getStatus() {
+        return status.get();
+    }
 
-  public SimpleStringProperty descriptionProperty() {
-    return description;
-  }
+    public SimpleStringProperty statusProperty() {
+        return status;
+    }
 
-  public String getStatus() {
-    return status.get();
-  }
+    public String getCompletedBy() {
+        return completedBy.get();
+    }
 
-  public SimpleStringProperty statusProperty() {
-    return status;
-  }
+    public SimpleStringProperty completedByProperty() {
+        return completedBy;
+    }
 
-  public String getCompletedBy() {
-    return completedBy.get();
-  }
+    @Override
+    public ArrayList<JFXTreeTableColumn<ITTicket, ?>> getColumns() {
 
-  public SimpleStringProperty completedByProperty() {
-    return completedBy;
-  }
+        JFXTreeTableColumn<ITTicket, String> column1 = new JFXTreeTableColumn<>("Time");
+        column1.setCellValueFactory(param -> param.getValue().getValue().ticketTimeProperty());
 
-  @Override
-  public ArrayList<JFXTreeTableColumn<ITTicket, ?>> getColumns() {
-    JFXTreeTableColumn<ITTicket, Integer> column1 = new JFXTreeTableColumn<>("Ticket #");
-    column1.setCellValueFactory(
-        param -> param.getValue().getValue().ticketNumProperty().asObject());
+        JFXTreeTableColumn<ITTicket, String> column2 = new JFXTreeTableColumn<>("Status");
+        column2.setCellValueFactory(param -> param.getValue().getValue().statusProperty());
 
-    JFXTreeTableColumn<ITTicket, String> column2 = new JFXTreeTableColumn<>("Time");
-    column2.setCellValueFactory(param -> param.getValue().getValue().ticketTimeProperty());
+        JFXTreeTableColumn<ITTicket, String> column3 = new JFXTreeTableColumn<>("Category");
+        column3.setCellValueFactory(param -> param.getValue().getValue().categoryProperty());
 
-    JFXTreeTableColumn<ITTicket, String> column3 = new JFXTreeTableColumn<>("Status");
-    column3.setCellValueFactory(param -> param.getValue().getValue().statusProperty());
+        JFXTreeTableColumn<ITTicket, String> column4 = new JFXTreeTableColumn<>("Location");
+        column4.setCellValueFactory(param -> param.getValue().getValue().locationProperty());
 
-    JFXTreeTableColumn<ITTicket, String> column4 = new JFXTreeTableColumn<>("Category");
-    column4.setCellValueFactory(param -> param.getValue().getValue().categoryProperty());
+        JFXTreeTableColumn<ITTicket, String> column5 = new JFXTreeTableColumn<>("Requester Name");
+        column5.setCellValueFactory(param -> param.getValue().getValue().nameProperty());
 
-    JFXTreeTableColumn<ITTicket, String> column5 = new JFXTreeTableColumn<>("Location");
-    column5.setCellValueFactory(param -> param.getValue().getValue().locationProperty());
+        JFXTreeTableColumn<ITTicket, String> column6 = new JFXTreeTableColumn<>("Employee Name");
+        column6.setCellValueFactory(param -> param.getValue().getValue().completedByProperty());
 
-    JFXTreeTableColumn<ITTicket, String> column6 = new JFXTreeTableColumn<>("Requester Name");
-    column6.setCellValueFactory(param -> param.getValue().getValue().nameProperty());
-
-    JFXTreeTableColumn<ITTicket, String> column7 = new JFXTreeTableColumn<>("Employee Name");
-    column7.setCellValueFactory(param -> param.getValue().getValue().completedByProperty());
-
-    return new ArrayList<>(List.of(column1, column2, column3, column4, column5, column6, column7));
-  }
+        return new ArrayList<>(List.of(column1, column2, column3, column4, column5, column6));
+    }
 }
--- src/main/java/edu/wpi/cs3733/d20/teamA/database/ITTicketDatabase.java
+++ src/main/java/edu/wpi/cs3733/d20/teamA/database/ITTicketDatabase.java
@@ -1,9 +1,18 @@
 package edu.wpi.cs3733.d20.teamA.database;
 
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.sql.*;
+import java.util.List;
 
 public class ITTicketDatabase extends Database {
-    private int ticketNum = 0;
+    private int ticketNum = getSizeITTickets() + 1;
 
     public ITTicketDatabase(Connection connection) {
         super(connection);
@@ -28,7 +37,7 @@ public class ITTicketDatabase extends Database {
         // Create the graph tables
         return helperPrepared(
                 "CREATE TABLE ITTickets (ticketNum INTEGER PRIMARY KEY, "
-                        + "timestamp VarChar(30) NOT NULL, "
+                        + "ticketTime TIMESTAMP NOT NULL, "
                         + "status VarChar(50) NOT NULL, "
                         + "category Varchar(50) NOT NULL, "
                         + "location Varchar(10) NOT NULL, "
@@ -39,25 +48,29 @@ public class ITTicketDatabase extends Database {
                         + "CONSTRAINT ITStatus CHECK (status in ('Ticket Sent', 'In Progress', 'Complete')))");
     }
 
-    public boolean addTicket(String category, String location, String name, String description) {
+    public boolean addTicket(
+            Timestamp ticketTime,
+            String status,
+            String category,
+            String location,
+            String name,
+            String completedBy,
+            String description) {
 
-        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-        String initStatus = "Ticket Sent";
-        String initCompletedBy = "";
         try {
             // creates the prepared statement that will be sent to the database
             PreparedStatement pstmt =
                     getConnection()
                             .prepareStatement(
-                                    "INSERT INTO ITTickets (ticketNum, timestamp , status, category, location, name, completedBy, description) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+                                    "INSERT INTO ITTickets (ticketNum, ticketTime , status, category, location, name, completedBy, description) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
             // sets all the parameters of the prepared statement string
             pstmt.setInt(1, ticketNum);
-            pstmt.setTimestamp(2, timestamp);
-            pstmt.setString(3, initStatus);
+            pstmt.setTimestamp(2, ticketTime);
+            pstmt.setString(3, status);
             pstmt.setString(4, category);
             pstmt.setString(5, location);
             pstmt.setString(6, name);
-            pstmt.setString(7, initCompletedBy);
+            pstmt.setString(7, completedBy);
             pstmt.setString(8, description);
             pstmt.executeUpdate();
             pstmt.close();
@@ -69,6 +82,96 @@ public class ITTicketDatabase extends Database {
             return false;
         }
     }
+
+    public int getSizeITTickets() {
+        return getSize("ITTickets");
+    }
+
+    public boolean changeStatus(int ticketNumber, String newStatus) {
+
+        try {
+            PreparedStatement pstmt =
+                    getConnection()
+                            .prepareStatement(
+                                    "UPDATE ITTickets Set status = '" + newStatus + "' WHERE orderNumber = " + ticketNumber);
+            pstmt.executeUpdate();
+            pstmt.close();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public ObservableList<ITTicket> ITTicketObservableList() {
+        ObservableList<ITTicket> ITTicketObservableList = FXCollections.observableArrayList();
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:derby:BWDatabase");
+            PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM ITTickets");
+            ResultSet rset = pstmt.executeQuery();
+            while (rset.next()) {
+                int ticketNum = rset.getInt("ticketNum");
+                Timestamp ticketTime = rset.getTimestamp("ticketTime");
+                String status = rset.getString("status");
+                String category = rset.getString("category");
+                String location = rset.getString("location");
+                String name = rset.getString("name");
+                String completedBy = rset.getString("completedBy");
+                String description = rset.getString("description");
+
+                ITTicket node =
+                        new ITTicket(ticketTime, status, category, location, name, completedBy, description);
+                ITTicketObservableList.add(node);
+            }
+            rset.close();
+            pstmt.close();
+            conn.close();
+            return ITTicketObservableList;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return ITTicketObservableList;
+        }
+    }
+
+    public void readITTicketsCSV() {
+        try {
+            InputStream stream =
+                    getClass().getResourceAsStream("/edu/wpi/cs3733/d20/teamA/csvfiles/ITTicketsCSV.csv");
+            CSVReader reader = new CSVReader(new InputStreamReader(stream));
+            List<String[]> data = reader.readAll();
+            for (int i = 1; i < data.size(); i++) {
+                Timestamp ticketTime;
+                String status, category, location, name, completedBy, description;
+                ticketTime = Timestamp.valueOf(data.get(i)[0]);
+                status = data.get(i)[2];
+                category = data.get(i)[3];
+                location = data.get(i)[4];
+                name = data.get(i)[5];
+                completedBy = data.get(i)[6];
+                description = data.get(i)[7];
+                addTicket(ticketTime, status, category, location, name, completedBy, description);
+            }
+        } catch (IOException | CsvException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //    public boolean changeOrderStatus(int orderNum, String newStat) {
+    //
+    //        try {
+    //            PreparedStatement pstmt =
+    //                    getConnection()
+    //                            .prepareStatement(
+    //                                    "UPDATE Orders Set status = '" + newStat + "' WHERE
+    // orderNumber = " + orderNum);
+    //            pstmt.executeUpdate();
+    //            pstmt.close();
+    //            return true;
+    //        } catch (SQLException e) {
+    //            e.printStackTrace();
+    //            return false;
+    //        }
+    //    }
 
     //  public ObservableList<ITTicket> ITTicketObervableList() {
     //    ObservableList<ITTicket> obervableITTicketList = FXCollections.observableArrayList();
