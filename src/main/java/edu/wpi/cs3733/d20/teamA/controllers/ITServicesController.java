0facab4f019f18ae6781b2c75855ebe2073db6e5
Gabriel Dudlicek <g.dudlicek@gmail.com>
2020-04-25 20:41:05 -0500

    started making event handlers in IT Service controller

--- src/main/java/edu/wpi/cs3733/d20/teamA/controllers/ITServicesController.java
+++ src/main/java/edu/wpi/cs3733/d20/teamA/controllers/ITServicesController.java
@@ -3,6 +3,7 @@ package edu.wpi.cs3733.d20.teamA.controllers;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
+import edu.wpi.cs3733.d20.teamA.graph.Node;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.layout.GridPane;
@@ -19,7 +20,41 @@ public class ITServicesController extends AbstractController {
   @FXML private JFXComboBox statusChangeStatus;
   @FXML private JFXTextField statusChangeName;
 
-  public void submitTicket(ActionEvent actionEvent) {}
+    public void initialize() {
+      if (itTicketDatabase.getSizeITTickets() == -1) {
+        itTicketDatabase.dropTables();
+        itTicketDatabase.createTables();
+        itTicketDatabase.readITTicketsCSV();
+      } else if (itTicketDatabase.getSizeITTickets() == 0) {
+        flDatabase.removeAllOrders();
+        flDatabase.removeAllFlowers();
+        flDatabase.readFlowersCSV();
+        flDatabase.readFlowerOrderCSV();
+      }
+    }
 
-  public void changeStatus(ActionEvent actionEvent) {}
+  public void deleteRequest(ActionEvent actionEvent) {
+  }
+
+  public void changeStatus(ActionEvent actionEvent) {
+  }
+
+  public void submitTicket(ActionEvent actionEvent) {
+//    Node node = roomList.getSelectionModel().getSelectedItem();
+//    String loc = "";
+//    if (node != null && !comboboxPriority.getValue().equals("")) {
+//      loc = node.getNodeID();
+//      janitorDatabase.addRequest(loc, comboboxPriority.getValue());
+//      statusHash.put(loc, "Not Started");
+//      comboboxPriority.getSelectionModel().clearSelection();
+//      labelSubmitRequest.setText("Request Submitted Successfully");
+//    } else if (comboboxPriority.getValue() != null && node == null) {
+//      labelSubmitRequest.setText("Please enter a location");
+//    } else if (comboboxPriority.getValue() == null && node != null) {
+//      labelSubmitRequest.setText("Please enter a priority");
+//    } else if (comboboxPriority.getValue() == null && node == null) {
+//      labelSubmitRequest.setText("Please enter data");
+//    }
+//    refreshActiveRequests();
+  }
 }
