f5ee56c3f7771f6084ff4653e45e5ec233c3a3af
dgcole <dyllancole@outlook.com>
2020-04-30 00:51:38 -0400

    New scene switcher start

--- src/main/java/edu/wpi/cs3733/d20/teamA/controllers/SceneSwitcherController.java
+++ src/main/java/edu/wpi/cs3733/d20/teamA/controllers/SceneSwitcherController.java
@@ -1,70 +1,139 @@
 package edu.wpi.cs3733.d20.teamA.controllers;
 
 import com.jfoenix.controls.JFXButton;
-import edu.wpi.cs3733.d20.teamA.controllers.dialog.NotificationController;
-import edu.wpi.cs3733.d20.teamA.util.DialogUtil;
-import edu.wpi.cs3733.d20.teamA.util.TabSwitchEvent;
+import edu.wpi.cs3733.d20.teamA.util.FXMLCache;
+import edu.wpi.cs3733.d20.teamA.util.ThreadPool;
+import java.util.Stack;
+import javafx.animation.TranslateTransition;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
-import org.kordamp.ikonli.fontawesome5.FontAwesomeRegular;
-import org.kordamp.ikonli.fontawesome5.FontAwesomeSolid;
-import org.kordamp.ikonli.javafx.FontIcon;
+import javafx.util.Duration;
 
 public class SceneSwitcherController {
-  @FXML private TabPane tabPane;
-  @FXML private Tab mapTab;
-  @FXML private Tab serviceTab;
-  @FXML private Tab employeeLoginTab;
-  @FXML private JFXButton informationButton;
-  @FXML private StackPane dialogPane;
+  @FXML private ImageView backgroundImage;
+  @FXML private StackPane rootPane;
+  @FXML private JFXButton backButton;
+  @FXML private GridPane contentPane;
+
+  private static SceneSwitcherController instance;
+
+  private Stack<Node> sceneStack;
+
+  private boolean transitioning;
 
   @FXML
   public void initialize() {
-    // Setup tab icons
-    mapTab.setGraphic(new FontIcon(FontAwesomeRegular.MAP));
-    serviceTab.setGraphic(new FontIcon(FontAwesomeSolid.CUBES));
-    employeeLoginTab.setGraphic(new FontIcon(FontAwesomeSolid.SIGN_IN_ALT));
-
-    // Settings button icon
-    informationButton.setGraphic(new FontIcon(FontAwesomeRegular.BELL));
-
-    // Setup dynamic tab resizing
-    tabPane
+    // Bind background image to just outside the bounds of the window for proper formatting
+    rootPane
         .widthProperty()
         .addListener(
             observable -> {
-              double newWidth = tabPane.getWidth() / tabPane.getTabs().size();
-              newWidth -= 25;
-
-              tabPane.setTabMinWidth(newWidth);
-              tabPane.setTabMaxWidth(newWidth);
+              backgroundImage.setFitWidth(rootPane.getWidth() + 50.0);
             });
 
-    tabPane
-        .getTabs()
-        .forEach(
-            tab -> {
-              tab.setOnSelectionChanged(
-                  event -> {
-                    if (tab.isSelected()) {
-                      Node node = tab.getContent();
-                      node.fireEvent(new TabSwitchEvent());
-                    }
-                  });
+    rootPane
+        .heightProperty()
+        .addListener(
+            observable -> {
+              backgroundImage.setFitHeight(rootPane.getHeight() + 50.0);
             });
+
+    // Set this equal to instance
+    instance = this;
+
+    // Setup scene stack
+    sceneStack = new Stack<>();
+
+    pushScene("views/NodeModifyPopup.fxml");
+    ThreadPool.runBackgroundTask(
+        () -> {
+          try {
+            Thread.currentThread().sleep(10000);
+          } catch (Exception e) {
+            e.printStackTrace();
+          }
+          Platform.runLater(
+              () -> {
+                SceneSwitcherController.pushScene("views/LaundryEdit.fxml");
+              });
+        });
+    //
+    // pushScene("views/MapEditor.fxml");
   }
 
   @FXML
-  public void informationButtonPressed() {
-    DialogUtil.complexDialog(
-        dialogPane,
-        "Announcements",
-        "views/NotificationWall.fxml",
-        true,
-        null,
-        new NotificationController());
+  public void pressedBack() {
+    if (!transitioning) {
+      popScene();
+    }
+  }
+
+  private void transition(boolean right) {
+    if (transitioning) {
+      return;
+    }
+    boolean first = contentPane.getChildren().isEmpty();
+
+    Node top = sceneStack.peek();
+    contentPane.getChildren().add(top);
+    if (!first) {
+      top.setTranslateX(right ? contentPane.getWidth() : -contentPane.getWidth());
+
+      transitioning = true;
+      TranslateTransition transOut =
+          new TranslateTransition(Duration.millis(500), contentPane.getChildren().get(0));
+      transOut.setByX(right ? -contentPane.getWidth() : contentPane.getWidth());
+      transOut.setOnFinished(
+          event -> {
+            contentPane.getChildren().remove(transOut.getNode());
+          });
+      transOut.play();
+
+      TranslateTransition transIn = new TranslateTransition(Duration.millis(500), top);
+      transIn.setByX(right ? -contentPane.getWidth() : contentPane.getWidth());
+      transIn.setOnFinished(
+          event -> {
+            transitioning = false;
+          });
+      transIn.play();
+    }
+
+    if (sceneStack.size() > 2) {
+      backButton.setText("Back");
+      backButton.setVisible(true);
+    } else if (sceneStack.size() == 2) {
+      backButton.setText("Home");
+      backButton.setVisible(true);
+    } else {
+      backButton.setVisible(false);
+    }
+  }
+
+  private static void pushScene(Node newNode) {
+    if (instance == null) {
+      return;
+    }
+
+    instance.sceneStack.push(newNode);
+    instance.transition(true);
+  }
+
+  private static Node popScene() {
+    if (instance == null || instance.sceneStack.size() <= 1) {
+      return null;
+    }
+
+    Node old = instance.sceneStack.pop();
+    instance.transition(false);
+    return old;
+  }
+
+  public static void pushScene(String fxmlPath) {
+    Node node = FXMLCache.loadFXML(fxmlPath);
+    pushScene(node);
   }
 }
