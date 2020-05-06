b320f3870b3100d748a2e341aa7705b74a8c6e0d
YashPatel5 <yppatel@wpi.edu>
2020-05-05 20:25:54 -0400

    Added new Employees

--- src/main/java/edu/wpi/cs3733/d20/teamA/controllers/SceneSwitcherController.java
+++ src/main/java/edu/wpi/cs3733/d20/teamA/controllers/SceneSwitcherController.java
@@ -87,11 +87,11 @@ public class SceneSwitcherController extends AbstractController {
       eDB.dropTables();
       eDB.createTables();
       eDB.addEmployee("111111", "Amethyst", "Asguardians", "admin", "admin", EmployeeTitle.ADMIN);
-      eDB.addEmployee("222222", "Yash", "Patel", "staff", "staff", EmployeeTitle.ADMIN);
+      eDB.addEmployee("222222", "Yash", "Patel", "staff", "staff", EmployeeTitle.JANITOR);
       eDB.addEmployee("Brennan", "Aubuchaun", "baub", "baUb578", EmployeeTitle.INTERPRETER);
-      eDB.addEmployee("Cory", "Helmuth", "CLHelmuth77", "!Lov3MyPiano", EmployeeTitle.NURSE);
+      eDB.addEmployee("Cory", "Helmuth", "CLHelmuth77", "!Lov3MyPiano2", EmployeeTitle.NURSE);
       eDB.addEmployee("Eva", "Labbe", "ELLabbe", "CluBP3nGuin3", EmployeeTitle.JANITOR);
-      eDB.addEmployee("Dean", "Winchester", "WinDean", "catNipRox", EmployeeTitle.DOCTOR);
+      eDB.addEmployee("Dean", "Winchester", "WinDean", "catNipRox2", EmployeeTitle.DOCTOR);
       eDB.addEmployee("Stella", "Simmons", "Ssimmons", "gaLaxY6", EmployeeTitle.RECEPTIONIST);
       eDB.addEmployee("Yolanda", "Daniels", "YDaniels", "SpoodRman3", EmployeeTitle.RETAIL);
       // create account with rfid
