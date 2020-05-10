060017c9645d7ec07b89515b0653671af04c3988
Gabriel Dudlicek <g.dudlicek@gmail.com>
2020-05-09 20:55:16 -0500

    updated About page

--- src/main/java/edu/wpi/cs3733/d20/teamA/controllers/SceneSwitcherController.java
+++ src/main/java/edu/wpi/cs3733/d20/teamA/controllers/SceneSwitcherController.java
@@ -592,6 +592,6 @@ public class SceneSwitcherController extends AbstractController {
   }
 
   public void openAbout(ActionEvent actionEvent) {
-    pushScene("views/AboutPage.fxml", TransitionType.ZOOM);
+    pushScene("views/About.fxml", TransitionType.ZOOM);
   }
 }
