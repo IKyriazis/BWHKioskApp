63ea9d57a97e3fc76eaff47e26b76c852f4a0de5
Gabriel Dudlicek <g.dudlicek@gmail.com>
2020-05-09 22:26:46 -0500

    updated about page and credits page

--- src/main/java/edu/wpi/cs3733/d20/teamA/controllers/nav/AboutPageController.java
+++ src/main/java/edu/wpi/cs3733/d20/teamA/controllers/nav/AboutPageController.java
@@ -1,11 +1,18 @@
 package edu.wpi.cs3733.d20.teamA.controllers.nav;
 
+import static edu.wpi.cs3733.d20.teamA.controllers.SceneSwitcherController.pushScene;
+
 import edu.wpi.cs3733.d20.teamA.controllers.SceneSwitcherController;
+import edu.wpi.cs3733.d20.teamA.controls.TransitionType;
 import javafx.event.ActionEvent;
 
 public class AboutPageController {
 
   public void exitAbout(ActionEvent actionEvent) {
     SceneSwitcherController.popScene();
+  }
+
+  public void openCredits(ActionEvent actionEvent) {
+    pushScene("views/Credits.fxml", TransitionType.ZOOM);
   }
 }
